a6a67f6f4a3c9a3f080e0cce1d02e86a3a23e6b4
Joaquin Origlia <joaquin.origlia@mi.unc.edu.ar>
2023-09-19 21:44:38 -0300

    Añadidos modelos de usuario,partida y carta

---
+++ database.py
@@ -0,0 +1,37 @@
+from pony.orm import *
+
+
+db = Database()
+
+
+class Jugador(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    # _table_ = 'Partida'
+    nombre = Required(str)
+    partida = Optional('Partida')
+
+
+class Partida(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    iniciado = Required(bool)
+    id_jugador_creador = Required(int)
+    minimo_jugadores = Required(int)
+    maximo_jugadores = Required(int)
+    contrasena = Optional(str)
+    jugadores = Set(Jugador)
+
+
+class Carta(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    nombre = Required(str)
+    numero_jugadores = Required(int)
+    tipo_dorso = Required(int)
+    tipo_de_accion = Required(str)
+    descripcion = Optional(str)
+
+
+# Conecta a la base de datos SQLite en el archivo 'database.sqlite'
+db.bind(provider='sqlite', filename='database.sqlite', create_db=True)
+
+# Genera las tablas en la base de datos
+db.generate_mapping(create_tables=True)
