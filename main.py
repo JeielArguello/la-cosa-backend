d85deac2008d25141a81924c35f57fbc18bc44b3
Joaquin Origlia <joaquin.origlia@mi.unc.edu.ar>
2023-09-20 16:20:49 -0300

    Las cartas se crean al iniciar el server

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from database import *
+from init_db import initialize_database
 
 app = FastAPI()
+
+
+@app.on_event("startup")
+def startup_db():
+    initialize_database()
 
 
 @app.get("/")
