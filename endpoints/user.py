365ea89ca8cb6868a953ce4f5336d8ea756b475c
Joaquin Origlia <joaquin.origlia@mi.unc.edu.ar>
2023-09-25 20:02:58 -0300

    Agregado Form al endpoint user

--- endpoints/user.py
+++ endpoints/user.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, status, Depends, APIRouter
+from fastapi import FastAPI, status, Depends, APIRouter, Form
 from fastapi.exceptions import HTTPException
 from pony.orm import Database
 from models.crud import *
@@ -6,13 +6,14 @@ from models.crud import *
 
 router = APIRouter()
 
-@router.post("/create", status_code = status.HTTP_201_CREATED)
-async def create_user(usuario: str, db: Database = Depends(get_db)):
+
+@router.post("/create", status_code=status.HTTP_201_CREATED)
+async def create_user(usuario: str = Form(), db: Database = Depends(get_db)):
     try:
         resultado = db_create_user(db, usuario)
         return resultado
     except Exception as e:
         raise HTTPException(
-            status_code = status.HTTP_400_BAD_REQUEST, 
+            status_code=status.HTTP_400_BAD_REQUEST,
             detail="Error al crear usuario."
         )
