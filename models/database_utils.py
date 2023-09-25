bcf339a7a9cd93564d977dfa4f36cd1d4c7d7a57
JoseIvanNieva <ivan.nva@gmail.com>
2023-09-25 10:35:49 -0300

    crear usuario

---
+++ models/database_utils.py
@@ -0,0 +1,14 @@
+from models.database import*
+#from database import Database, db_session, db
+from typing import Dict
+
+
+def get_db() -> Database:
+    return db
+
+@db_session
+def db_create_user(db: Database, nombre: str) -> Dict[str, int]:
+    user_in_db = db.Jugador(nombre = nombre)
+    user_in_db.flush()
+    result = {"user_name": user_in_db.nombre, "id": user_in_db.id}
+    return result
