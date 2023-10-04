58f596d13b08ec4f969acbc63ffd15c49b7a8f70
Joaquin Origlia <joaquin.origlia@mi.unc.edu.ar>
2023-10-04 12:15:02 -0300

    Jugar lanzallamas inicial

---
+++ logic/action_effects.py
@@ -0,0 +1,36 @@
+from game import Juego
+
+
+def play_lanzallamas(atacante_in: int, objetivo_in: int, juego: Juego):
+    # get Jugador objetivo
+    for jugador in juego.jugadores_en_partida:
+        if jugador.id == objetivo_in:
+            objetivo = jugador
+    # get indices
+    indice_objetivo = juego.posiciones.index(objetivo_in)
+    indice_atacante = juego.posiciones.index(atacante_in)
+    indice_posicion_intermedia = get_posicion_intermedia(
+        juego.posiciones, indice_objetivo, indice_atacante)
+    # check obstaculos
+    if juego.posiciones[indice_posicion_intermedia] != 0:
+        pass
+    # check defensa
+    # elif defensa:
+    #     pass
+    # eliminar jugador y posiciones
+    else:
+        juego.jugadores_en_partida.remove(objetivo)
+        del juego.posiciones[max(indice_objetivo, indice_posicion_intermedia)]
+        del juego.posiciones[min(indice_objetivo, indice_posicion_intermedia)]
+
+
+def get_posicion_intermedia(posiciones, indice_jugador1, indice_jugador2):
+    border_one = (indice_jugador1 == 0 and indice_jugador2 ==
+                  len(posiciones) - 1)
+    border_two = (indice_jugador2 == 0 and indice_jugador1 ==
+                  len(posiciones) - 1)
+    if (border_one or border_two):
+        posicion = len(posiciones) - 1
+    else:
+        posicion = min(indice_jugador1, indice_jugador2) + 1
+    return posicion
