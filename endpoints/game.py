f9d2e1e66771ca4baf46b3b3848e3f4fb739091e
Joaquin Origlia <joaquin.origlia@mi.unc.edu.ar>
2023-10-08 11:51:38 -0300

    Esqueleto incial de endpoints nuevos

---
+++ endpoints/game.py
@@ -0,0 +1,109 @@
+from fastapi import HTTPException, Body
+from fastapi import APIRouter, Form, HTTPException, status, WebSocket
+# from models.match_models import Match, all_matchs
+from models.crud import *
+from models.database_utils import *
+from utils.match_utils import *
+from models.match_models import *
+from endpoints.websocket import broadcast, ws_players_list
+from logic.game import Juego
+from logic.player import JugadorPartida
+from logic.action_effects import play_lanzallamas
+
+router = APIRouter()
+
+######
+# Robar Carta
+######
+
+
+@router.post('/pick')
+async def pick_a_card_from_deck(match_id: int, player_id: int):
+    juego = get_global_juego(match_id)
+    for jugador_en_partida in juego.jugadores_en_partida:
+        if jugador_en_partida.id == player_id:
+            jugador = jugador_en_partida
+    robar_carta(juego, jugador)
+
+
+def robar_carta(juego: Juego, jugador: JugadorPartida):
+    if len(juego.mazo) == 0:
+        raise HTTPException(
+            status_code=400,
+            detail="El mazo esta vacio")
+    carta_id = juego.mazo.pop()
+    jugador.agregar_carta(carta_id)
+
+######
+# Jugar carta
+######
+
+
+@router.post('/play')
+async def jugar_carta(match_id: int, card_id: int, player_objective: int, player_orig: int):
+    juego = get_global_juego(match_id)
+    resultado = jugar_la_carta(juego, card_id, player_objective, player_orig)
+    if descartar_carta(card_id, player_orig, match_id):
+        return {"carta": card_id, "jugada contra": player_objective, "por": player_orig}
+    return {"error al jugar la carta": card_id, "contra": player_objective, "por": player_orig}
+
+
+def jugar_la_carta(juego: Juego, card_id: int, player_objective: int, player_orig: int):
+    if card_id in [22, 23, 24, 25, 26]:
+        play_lanzallamas(player_orig, player_objective, juego)
+        return True
+    else:
+        return False
+
+
+@db_session
+def descartar_carta(card_id: int, player_orig: int, match_id: int):
+    juego = select(j for j in Partida if j.id == match_id).first()
+    if juego:
+        juego.mazo_descarte.append(card_id)
+        juego.jugadores_en_partida[player_orig].cartas.pop(card_id)
+        return True
+    return False
+
+######
+# Finalizar Partida
+######
+
+
+@router.post('/finish')
+async def finish_match(match_id: int):
+    juego = get_global_juego(match_id)
+    result = finalizar_partida(juego)
+    delete_global_juego(match_id)
+    return result
+
+
+def finalizar_partida(juego: Juego):
+    if len(juego.cantidad_jugadores) == 1:
+        ganador = juego.jugadores_en_partida.pop()
+        ganador_id = ganador.id
+        return {"mensaje": "La partida ha finalizado", "ganador": ganador_id}
+    elif len(juego.cantidad_jugadores) == 0:
+        return {"mensaje": "partida sin jugadores", "ganador": 0}
+    else:
+        return {"mensaje": "La partida aún no ha finalizado", "ganador": 0}
+
+
+######
+# Funcion que obtiene juego
+######
+global_juegos = list[Juego]
+
+
+def get_global_juego(match_id: int) -> Juego:
+    for juego in global_juegos:
+        if juego.id == match_id:
+            result = juego
+    return result
+
+
+def delete_global_juego(match_id):
+    for juego in global_juegos:
+        if juego.id == match_id:
+            result = juego
+    global_juegos.remove(result)
