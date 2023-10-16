f699fec263f1f3713c83edf3fc1a5c65dce326be
Joaquin Origlia <joaquin.origlia@mi.unc.edu.ar>
2023-10-15 21:40:55 -0300

    Pequeña correccion en broadcast del lobby

--- models/lobby_models.py
+++ models/lobby_models.py
@@ -49,7 +49,6 @@ class Lobby:
 
     async def broadcast_lobby(self, message: dict):
         for p in self.ws_players:
-            await p.send_json("R")
             await p.send_json(message)
             await p.send_json("reset")
 
