095dfe84cc0bc7ed60613d1990e1fed6229eaae5
JeielArguello <JeielArguello@bitbucket.org>
2023-11-06 20:32:57 -0300

    Agregando test

--- tests/test_logic/test_defense_effects.py
+++ tests/test_logic/test_defense_effects.py
@@ -1,5 +1,5 @@
 from fastapi.testclient import TestClient
-from unittest.mock import Mock
+from unittest.mock import AsyncMock, Mock
 
 import pytest
 from models.game import Juego
@@ -13,7 +13,7 @@ client = TestClient(app)
 
 mocker = Mock()
 
-
+mocker2 = AsyncMock()
 
 @pytest.fixture
 def mock_atacante(mocker):
@@ -27,9 +27,22 @@ def mock_atacante(mocker):
 def mock_objetivo(mocker):
     mocker.patch("models.player.get_name", return_value="pedro")
     jugador = JugadorPartida(id=2)
-    jugador.cartas=[5,6,7,8]
+    jugador.cartas=[5,6,77,78]
     return jugador
 
+@pytest.fixture
+def mock_proximo(mocker):
+    mocker.patch("models.player.get_name", return_value="pedro")
+    jugador = JugadorPartida(id=3)
+    jugador.cartas=[53,61,7,71]
+    return jugador
+
+@pytest.fixture
+def mock_proximo2(mocker):
+    mocker.patch("models.player.get_name", return_value="pedro")
+    jugador = JugadorPartida(id=4)
+    jugador.cartas=[53,61,7,71]
+    return jugador
 
 @pytest.fixture
 def mock_juego(mocker):
@@ -41,7 +54,16 @@ def mock_juego(mocker):
     return juego
 
 @pytest.fixture
-def mock_card_id(mocker):
+def mock_juego_async(mocker):
+    mocker.patch("models.player.get_name", return_value="pepe")
+    juego = Juego(partida_id=1, cantidad_jugadores=4,
+                  creador=1, jugadores_id=[1, 3, 2, 4])
+    juego.name = "test"
+    juego.posiciones = [1, 0, 2, 0, 3, 0, 4, 0]
+    return juego
+
+@pytest.fixture
+def mock_card_id():
     card_id = 75
     return card_id
 
@@ -82,3 +104,25 @@ def test_defensa_aterrador_fail(mock_juego, mock_atacante, mock_objetivo, mock_c
         defense_aterrador(mock_juego, mock_atacante, mock_objetivo, mock_card_id)
     assert exc.value.status_code == 400
     assert exc.value.detail == "No hay solicitud de intercambio"
+
+@pytest.mark.asyncio
+async def test_defensa_fallaste_success(mocker,mock_juego_async, mock_atacante, mock_objetivo, mock_card_id, mock_proximo, mock_proximo2):
+    mock_intercambio = IntercambiarCarta(mock_objetivo, mock_card_id, mock_atacante)
+    mock_juego_async.solicitud_intercambio = mock_intercambio
+    mock_juego_async.jugadores_en_partida = [mock_atacante, mock_objetivo, mock_proximo, mock_proximo2]
+    mock_card_id = 78
+    mock_objetivo.cartas = [5, 6, 78, 8]
+    mocker.patch("logic.defense_effects.Juego.mensaje_personal", return_value=None)
+    msg = await defensa_fallaste(mock_juego_async, mock_objetivo, mock_atacante, mock_card_id)
+    assert msg == {"mensaje": "pedro jugó carta fallaste contra pepe"}
+
+@pytest.mark.asyncio
+async def test_defensa_fallaste_fail(mock_juego_async, mock_atacante, mock_objetivo, mock_card_id, mock_proximo, mock_proximo2):
+    mock_intercambio = None
+    mock_juego_async.solicitud_intercambio = mock_intercambio
+    mock_juego_async.jugadores_en_partida = [mock_atacante, mock_objetivo, mock_proximo, mock_proximo2]
+
+    with pytest.raises(HTTPException) as exc:
+        await defensa_fallaste(mock_juego_async, mock_objetivo, mock_atacante, mock_card_id)
+    assert exc.value.status_code == 400
+    assert exc.value.detail == "No hay solicitud de intercambio."
